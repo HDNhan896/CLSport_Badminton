aac04c29e3d9cc62a37939eba003a50b3140434d
Van-Datt <vandat7sao@gmail.com>
2026-05-03 18:43:34 +0700

    add_js

---
+++ frontend/js/accout.js
@@ -0,0 +1,61 @@
+
+document.getElementById("loginForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    let user = document.getElementById("username").value.trim();
+    let pass = document.getElementById("password").value.trim();
+
+    let errorUser = document.getElementById("errorUser");
+    let errorPass = document.getElementById("errorPass");
+
+    let isValid = true;
+
+    // Reset lỗi
+    errorUser.innerText = "";
+    errorPass.innerText = "";
+
+    // Kiểm tra username
+    if (user === "") {
+        errorUser.innerText = "Vui lòng nhập email hoặc số điện thoại";
+        isValid = false;
+    } else {
+        // Regex email
+        let emailRegex = /^[a-zA-Z0-9._%+-]+@gmail\.com$/;
+        // Regex số điện thoại 
+        let phoneRegex = /^0\d{9}$/;
+
+        if (!emailRegex.test(user) && !phoneRegex.test(user)) {
+            errorUser.innerText = "Email hoặc số điện thoại không hợp lệ";
+            isValid = false;
+        }
+    }
+
+    // Kiểm tra mật khẩu
+    if (pass === "") {
+        errorPass.innerText = "Vui lòng nhập mật khẩu";
+        isValid = false;
+    } else if (pass.length < 6) {
+        errorPass.innerText = "Mật khẩu phải có ít nhất 6 ký tự";
+        isValid = false;
+    }
+
+    // Nếu hợp lệ
+    if (isValid) {
+        alert("Đăng nhập thành công");
+    }
+});
+
+const toggleBtn = document.querySelector(".fa-eye-slash");
+const passwordInput = document.getElementById("password");
+
+toggleBtn.addEventListener("click", () => {
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+        toggleBtn.classList.remove("fa-eye-slash");
+        toggleBtn.classList.add("fa-eye");
+    } else {
+        passwordInput.type = "password";
+        toggleBtn.classList.add("fa-eye-slash");
+        toggleBtn.classList.remove("fa-eye");
+    }
+});
---
+++ frontend/js/signup.js
@@ -0,0 +1,69 @@
+
+document.getElementById("signupForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    let name = document.getElementById("name").value.trim();
+    let email = document.getElementById("email").value.trim();
+    let phone = document.getElementById("phone").value.trim();
+    let pass = document.getElementById("password").value.trim();
+    let repass = document.getElementById("repassword").value.trim();
+
+    let isValid = true;
+
+    // reset lỗi
+    document.querySelectorAll(".text-danger").forEach(e => e.innerText = "");
+
+    // regex
+    let nameRegex = /^([A-Z][a-z]*)(\s[A-Z][a-z]*)*$/;
+    let emailRegex = /^[a-zA-Z0-9._%+-]+@gmail\.com$/;
+    let phoneRegex = /^0\d{9}$/;
+    let passRegex = /^[\x00-\x7F]{6,15}$/;
+
+   // TÊN
+    if (name === "") {
+        document.getElementById("errorName").innerText = "Vui lòng nhập tên";
+        isValid = false;
+    } else if (!nameRegex.test(name)) {
+        document.getElementById("errorName").innerText =
+            "Mỗi từ phải viết hoa chữ cái đầu và không dấu (VD: Nguyen Van A)";
+        isValid = false;
+    }
+
+    // EMAIL
+    if (email === "") {
+        document.getElementById("errorEmail").innerText = "Vui lòng nhập email";
+        isValid = false;
+    } else if (!emailRegex.test(email)) {
+        document.getElementById("errorEmail").innerText = "Email phải có đuôi @gmail.com";
+        isValid = false;
+    }
+
+    // PHONE
+    if (phone !== "" && !phoneRegex.test(phone)) {
+        document.getElementById("errorPhone").innerText = "SĐT phải 10 số và bắt đầu bằng 0";
+        isValid = false;
+    }
+
+    // PASSWORD
+    if (pass === "") {
+        document.getElementById("errorPass").innerText = "Vui lòng nhập mật khẩu";
+        isValid = false;
+    } else if (!passRegex.test(pass)) {
+        document.getElementById("errorPass").innerText = "Mật khẩu 6-15 ký tự, không dấu";
+        isValid = false;
+    }
+
+    // NHẬP LẠI PASSWORD
+    if (repass === "") {
+        document.getElementById("errorRePass").innerText = "Vui lòng nhập lại mật khẩu";
+        isValid = false;
+    } else if (repass !== pass) {
+        document.getElementById("errorRePass").innerText = "Mật khẩu không khớp";
+        isValid = false;
+    }
+
+   //
+    if (isValid) {
+        alert("Đăng ký thành công");
+    }
+});
